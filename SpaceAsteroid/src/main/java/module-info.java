69a88050b207e01caa4ecec4c1c6fd8b08b5c963
watterfish <103984582+watterfish@users.noreply.github.com>
2022-12-19 21:11:32 +0800

    update some javafx file

--- SpaceAsteroid/src/main/java/module-info.java
+++ SpaceAsteroid/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.vexed.vexed {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
+    requires javafx.graphics;
 
     opens com.comp2059.app to javafx.fxml;
     exports com.comp2059.app;
