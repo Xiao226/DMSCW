cc5a7b51adb57baeaa5b98b17268676782c5f47c
watterfish <103984582+watterfish@users.noreply.github.com>
2022-12-21 19:00:09 +0800

    problem with return function

--- SpaceAsteroid/src/main/java/com/comp2059/app/SpaceAsteroidApplication.java
+++ SpaceAsteroid/src/main/java/com/comp2059/app/SpaceAsteroidApplication.java
@@ -6,14 +6,61 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import static com.comp2059.app.SpaceAsteroidController.initialTheStage;
+import java.io.IOException;
+
+import static com.comp2059.app.SpaceAsteroidController.stage;
 
 public class SpaceAsteroidApplication extends Application{
+    public static Parent startPage;
+    public static Parent endPage;
+    public static Parent ruleAndNamePage;
+    public static Parent rankingPage;
+    public static Parent skinPage;
+    public static Parent backgroundPage;
+    public static Parent gamePage;
     @Override
     public void start(Stage primaryStage) throws Exception{
-        initialTheStage(primaryStage);
+        initialPages();
     }
+    public void initialPages() throws IOException {
+        initialStartPage();
+        initialRuleAndNamePage();
+        initialGamePage();
+        initialRankingPage();
+        initialSkinPage();
+        initialBackgroundPage();
+        initialEndPage();
+    }
+    public void initialStartPage() throws IOException {
+        startPage = FXMLLoader.load(SpaceAsteroidController.class.getResource("JavaFX/startPage.fxml"));
+        stage.setTitle("Welcome to F2A's Game");
+        stage.setScene(new Scene(startPage, 1000, 700));// size of the window
+        stage.show();
+    }
+    public void initialEndPage()throws IOException {
+        endPage=FXMLLoader.load(getClass().getResource("JavaFX/EndPage.fxml"));
 
+    }
+    public void initialRuleAndNamePage()throws IOException {
+        ruleAndNamePage=FXMLLoader.load(getClass().getResource("JavaFX/RuleAndNamePage.fxml"));
+
+    }
+    public void initialRankingPage()throws IOException {
+        rankingPage= FXMLLoader.load(getClass().getResource("JavaFX/ranking.fxml"));
+
+    }
+    public void initialSkinPage()throws IOException {
+        skinPage=FXMLLoader.load(getClass().getResource("JavaFX/spaceShipSkin.fxml"));
+
+    }
+    public void initialBackgroundPage()throws IOException {
+        backgroundPage=FXMLLoader.load(getClass().getResource("JavaFX/backgroundChange.fxml"));
+
+    }
+    public void initialGamePage()throws IOException {
+        gamePage = FXMLLoader.load(getClass().getResource("JavaFX/gamePage.fxml"));
+
+    }
 
     public static void main(String[] args) {
         launch(args);
--- SpaceAsteroid/src/main/java/com/comp2059/app/SpaceAsteroidController.java
+++ SpaceAsteroid/src/main/java/com/comp2059/app/SpaceAsteroidController.java
@@ -19,63 +19,51 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
 
+import static com.comp2059.app.SpaceAsteroidApplication.*;
+
 public class SpaceAsteroidController {
-    private static Stage stage=new Stage();
+    public static Stage stage=new Stage();
     
-    @FXML
-    public static void initialTheStage(Stage primaryStage) throws IOException {
-        Parent startPage = FXMLLoader.load(SpaceAsteroidController.class.getResource("JavaFX/startPage.fxml"));
-        stage.setTitle("Welcome to F2A's Game");
-        stage.setScene(new Scene(startPage, 1000, 700));// size of the window
-        stage.show();
-    }
+
     @FXML
     public void onClickMoveToNameAndRule(MouseEvent mouseEvent) throws IOException {
-        Parent next = FXMLLoader.load(getClass().getResource("JavaFX/RuleAndNamePage.fxml"));
         stage.setTitle("Read the Rule and Enter Your Name");
-        stage.setScene(new Scene(next, 1000, 700));// size of the window
+        stage.setScene(new Scene(ruleAndNamePage, 1000, 700));// size of the window
     }
     @FXML
     public void onClickMoveToSkinChange(MouseEvent mouseEvent) throws IOException {
-        Parent next = FXMLLoader.load(getClass().getResource("JavaFX/spaceShipSkin.fxml"));
         stage.setTitle("Change your ship's skin here");
-        stage.setScene(new Scene(next, 1000, 700));// size of the window
+        stage.setScene(new Scene(skinPage, 1000, 700));// size of the window
     }
     @FXML
     public void onClickMoveToBackgroundChange(MouseEvent mouseEvent) throws IOException {
-        Parent next = FXMLLoader.load(getClass().getResource("JavaFX/backgroundChange.fxml"));
         stage.setTitle("Change your background here");
-        stage.setScene(new Scene(next, 1000, 700));// size of the window
+        stage.setScene(new Scene(backgroundPage, 1000, 700));// size of the window
     }
     @FXML
     public void onClickMoveToRanking(MouseEvent mouseEvent) throws IOException {
-        Parent next = FXMLLoader.load(getClass().getResource("JavaFX/ranking.fxml"));
         stage.setTitle("Ranking list");
-        stage.setScene(new Scene(next, 1000, 700));// size of the window
+        stage.setScene(new Scene(rankingPage, 1000, 700));// size of the window
     }
     @FXML
     public void onClickMoveToPlay(MouseEvent mouseEvent) throws IOException {
-        Parent next = FXMLLoader.load(getClass().getResource("JavaFX/gamePage.fxml"));
         stage.setTitle("This is your first try, have fun.");
-        stage.setScene(new Scene(next, 1000, 700));// size of the window
+        stage.setScene(new Scene(gamePage, 1000, 700));// size of the window
     }
     @FXML
     public void onClickMoveToEndPage(MouseEvent mouseEvent) throws IOException {
-        Parent next = FXMLLoader.load(getClass().getResource("JavaFX/EndPage.fxml"));
         stage.setTitle("Whoops you failed");
-        stage.setScene(new Scene(next, 1000, 700));// size of the window
+        stage.setScene(new Scene(endPage, 1000, 700));// size of the window
     }
     @FXML
     public void onClickReMoveToGame(MouseEvent mouseEvent) throws IOException {
-        Parent next = FXMLLoader.load(getClass().getResource("JavaFX/gamePage.fxml"));
         stage.setTitle("welcome back to the game again");// try to add a counter here
-        stage.setScene(new Scene(next, 1000, 700));// size of the window
+        stage.setScene(new Scene(gamePage, 1000, 700));// size of the window
     }
     @FXML
     public void onClickMoveToStartPage(MouseEvent mouseEvent) throws IOException {
-        Parent next = FXMLLoader.load(getClass().getResource("JavaFX/startPage.fxml"));
         stage.setTitle("Welcome to F2A's game");
-        stage.setScene(new Scene(next, 1000, 700));// size of the window
+        stage.setScene(new Scene(startPage, 1000, 700));// size of the window
     }
     @FXML
     public void shotDown(MouseEvent mouseEvent) throws IOException{
